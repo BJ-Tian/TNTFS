2ac7ccf670da30f6765223b9437cba3a0a10bba3
tian <tianchunren@haiqiu.com>
2023-04-14 11:37:39 +0800

    upload

---
+++ README.md
@@ -0,0 +1,30 @@
+# TNTFS
+
+MacOS M1 开源NTFS
+
+Python3.10 使用 py2app 打包编译
+
+理论上 MacOS 全系列通用
+
+#### 安装 macfuse
+
+关闭SIP保护特性
+
+系统中启用 macfuse
+
+```shell
+brew install ntfs-3g-mac
+brew install macfuse
+```
+
+#### 运行
+
+把软件放到应用程序里然后双击运行
+
+#### 开机自启
+
+<img width="343" alt="WechatIMG987" src="https://user-images.githubusercontent.com/130722656/231932672-c3c13010-8bff-46c8-917e-c61af317d709.png">
+
+#### PS
+
+TNTFS.app 是打包好的程序，可直接运行。
---
+++ ntfs.py
@@ -0,0 +1,60 @@
+from tkinter import *
+from tkinter import ttk
+import subprocess
+
+class FeetToMeters:
+
+    def __init__(self, root):
+        root.title("TNTFS")
+        mainframe = ttk.Frame(root, padding="5 5 15 15")
+        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        ttk.Label(mainframe, text="TNTFS Is Runing!").grid(column=2, row=1, sticky=S)
+        ttk.Button(mainframe, text='最小化', command=root.iconify).grid(column=1, row=2, sticky=E)
+        ttk.Button(mainframe, text='退出', command=root.quit).grid(column=3, row=2, sticky=W)
+        root.iconify()
+        root.after(1000, self.main)
+
+    def shell_ini(self,command):
+        res = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8',
+                            timeout=30,
+                            executable='/bin/bash')
+        if res.returncode == 0:
+            return(res.stdout)
+        else:
+            return(False)
+
+    def find_ntfs(self):
+        return_list = []
+        resp = self.shell_ini('df -h | grep ntfs://')
+        if type(resp) == bool and resp == False:
+            pass
+            # print("没有NTFS")
+        else:
+            for i in (resp.strip("\n").split("\n")):
+                USB_disk = "/dev/" + i.strip("\n").split(" ")[0].split("/")[2]
+                USB_Volumes = i.strip("\n").split(" ")[-1]
+                return_list.append([USB_disk,USB_Volumes])
+        return(return_list)
+
+    def main(self):
+        ntfs_list = self.find_ntfs()
+        for r in ntfs_list:
+            USB_info = {}
+            USB_info[r[0]] = {"Volumes":r[1]}
+            disk_status = self.shell_ini('ls ' + r[0])
+            if disk_status:
+                umount = self.shell_ini('sudo umount ' + USB_info[r[0]]["Volumes"])
+                # print("正在卸载",umount)
+                mount_shell = "sudo /System/Volumes/Data/opt/homebrew/bin/ntfs-3g {} {} -olocal -oallow_other -o auto_xattr".format(r[0],"/Volumes/" + str(USB_info[r[0]]["Volumes"]).split("/")[-1])
+                # print(mount_shell)
+                mount = self.shell_ini(mount_shell)
+                # print("正在挂载.",mount)
+            else:
+                pass
+                # print("磁盘不存在.")
+        root.after(1000, self.main)
+
+
+root = Tk()
+FeetToMeters(root)
+root.mainloop()
